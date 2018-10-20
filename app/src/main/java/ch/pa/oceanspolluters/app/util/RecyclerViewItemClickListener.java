37d823974c4d7c11e071ba14725f78b2d4e6d41c
Pierre Anken <pierre.anken@gmail.com>
2018-10-20 08:44:18 +0200

    Language level

---
+++ app/src/main/java/ch/pa/oceanspolluters/app/util/RecyclerViewItemClickListener.java
@@ -0,0 +1,8 @@
+package ch.pa.oceanspolluters.app.util;
+
+import android.view.View;
+
+public interface RecyclerViewItemClickListener {
+    void onItemClick(View v, int position);
+    void onItemLongClick(View v, int position);
+}
