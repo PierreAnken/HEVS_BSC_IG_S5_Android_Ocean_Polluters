5ced242a2afeadc86fb69339b374f18a39420cc6
Olivier Gottet <olivier.gottet@students.hevs.ch>
2018-11-09 09:00:14 +0100

    Updated AdministratorHomeActivity

--- app/src/main/java/ch/pa/oceanspolluters/app/ui/AdministratorHomeActivity.java
+++ app/src/main/java/ch/pa/oceanspolluters/app/ui/AdministratorHomeActivity.java
@@ -8,6 +8,8 @@ import ch.pa.oceanspolluters.app.R;
 
 public class AdministratorHomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "AdministratorActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
