f4541dc7d9b178c19aceccc3bb49604c79fabba7
Olivier Gottet <olivier.gottet@students.hevs.ch>
2018-11-11 21:23:05 +0100

    Docker debugging

--- app/src/main/java/ch/pa/oceanspolluters/app/ui/DockerShipContainerViewActivity.java
+++ app/src/main/java/ch/pa/oceanspolluters/app/ui/DockerShipContainerViewActivity.java
@@ -3,84 +3,103 @@ package ch.pa.oceanspolluters.app.ui;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ch.pa.oceanspolluters.app.R;
+import ch.pa.oceanspolluters.app.adapter.RecyclerAdapter;
 import ch.pa.oceanspolluters.app.database.pojo.ContainerWithItem;
 import ch.pa.oceanspolluters.app.database.pojo.ShipWithContainer;
+import ch.pa.oceanspolluters.app.model.Container;
+import ch.pa.oceanspolluters.app.util.OperationMode;
+import ch.pa.oceanspolluters.app.util.RecyclerViewItemClickListener;
+import ch.pa.oceanspolluters.app.util.TB;
 import ch.pa.oceanspolluters.app.viewmodel.ContainerListViewModel;
+import ch.pa.oceanspolluters.app.viewmodel.ContainerViewModel;
 import ch.pa.oceanspolluters.app.viewmodel.ShipListViewModel;
+import ch.pa.oceanspolluters.app.viewmodel.ShipViewModel;
 
 public class DockerShipContainerViewActivity extends AppCompatActivity {
 
     private ContainerWithItem mContainerWithItem;
-    private ContainerListViewModel mContainerListViewModel;
+    private ContainerViewModel mContainerListViewModel;
 
-    private ShipListViewModel mShipListModel;
-    private ArrayAdapter<String> shipAdapter;
-    private List<ShipWithContainer> mShips;
+    private ShipViewModel mViewModel;
+    private ShipWithContainer mShip;
 
     private TextView dockPosition;
     private TextView containerName;
-    private TextView shipNames;
-    private ToggleButton loadingStatus;
-    private TextView items;
+    private TextView loadingStatus;
+    private TextView dockerContainerListTitle;
+    private List<ContainerWithItem> mShipWithContainer;
 
     private static final String TAG = "dockerShipContViewAct";
+    private RecyclerAdapter<ContainerWithItem> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_docker_container_list);
 
-        Intent containerDetail = getIntent();
-        int shipId = Integer.parseInt(containerDetail.getStringExtra("shipId"));
-        Log.d(TAG, "PA_Debug received container id from intent:" + shipId);
+        Intent shipDetail = getIntent();
+        int shipId = Integer.parseInt(shipDetail.getStringExtra("shipId"));
 
-        // get container and display it
-        ContainerListViewModel.FactoryContainers factory = new ContainerListViewModel.FactoryContainers(getApplication(), shipId);
-        mContainerListViewModel = ViewModelProviders.of(this, factory).get(ContainerListViewModel.class);
-        mContainerListViewModel.getContainers().observe(this, cont -> {
-            if (cont != null) {
-                mContainerWithItem = cont;
-                Log.d(TAG, "PA_Debug container id from factory:" + ((ContainerWithItem) cont).container.getId());
+        //get ship and display it
+        ShipViewModel.FactoryShip factory = new ShipViewModel.FactoryShip(getApplication(), shipId);
+        mViewModel = ViewModelProviders.of(this, factory).get(ShipViewModel.class);
+        mViewModel.getShip().observe(this, ship -> {
+            if (ship != null) {
+                mShip = ship;
+                Log.d(TAG, "PA_Debug ship id from factory:" + ship.ship.getId());
                 updateView();
+
             }
         });
+
+        RecyclerView recyclerView = findViewById(R.id.dockerShipContainersRecyclerView);
+
+        mAdapter = new RecyclerAdapter<>(new RecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+            }
+
+            @Override
+            public void onItemLongClick(View v, int position) {
+            }
+        });
+
+        // generate new linear layout
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        TextView shipName = findViewById(R.id.docker_container_list_title);
+        shipName.setText("Test");
+
+//        ContainerListViewModel.FactoryContainers factory2 = new ContainerListViewModel.FactoryContainers(getApplication(), mShip.ship.getId());
+//        ContainerListViewModel mContainerForDocker = ViewModelProviders.of(this, factory2).get(ContainerListViewModel.class);
+//        mContainerForDocker.getContainers().observe(this, cont -> {
+//            if (cont != null) {
+//                mShipWithContainer = cont;
+//                mAdapter.setData(mShipWithContainer);
+//            }
+//        });
+        recyclerView.setAdapter(mAdapter);
     }
 
-    private void updateView(){
+    private void updateView() {
         Log.d(TAG, "PA_Debug updateView");
-
-        if(mShips != null){
-            String[] shipsNames = new String[mShips.size()];
-
-            for(int i = 0; i<shipsNames.length; i++){
-                shipsNames[i] = mShips.get(i).ship.getName();
-            }
-        }
-
-        if(mContainerWithItem != null){
-            containerName = findViewById(R.id.container_name);
-            dockPosition = findViewById(R.id.container_dock_position);
-            loadingStatus = findViewById(R.id.v_lm_loaded_status);
-            shipNames = findViewById(R.id.container_ship_name);
-            containerName.setText(mContainerWithItem.container.getName());
-            dockPosition.setText(mContainerWithItem.container.getDockPosition());
-            loadingStatus.setChecked(mContainerWithItem.container.getLoaded());
-            shipNames.setText(Integer.toString(mContainerWithItem.container.getShipId()));
-        }
-
-            items = findViewById(R.id.container_items_quantity_weight);
-            items.setText(mContainerWithItem.getWeight() +"kg / "+ mContainerWithItem.items.size() + " items");
 
     }
 
